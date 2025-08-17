30b5d129aae70a235cce936c8c288070713121c3
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-08-17 13:45:30 +0500

    fetch todos page

--- nextjs-todo-agentic-ui/src/app/page.tsx
+++ nextjs-todo-agentic-ui/src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import { apiCallGet, apiCallPost } from "@/api/api_call";
+import TodoList from "@/components/TodoList";
+import { AppRoutes } from "@/constant/AppRoutes";
+import { TodoListSchema } from "@/TypingSchemas/TodoListSchema";
 
-export default function Home() {
+export default async function Home() {
+  const data = await apiCallGet(AppRoutes.allTodos)
+
+
+  const todos: TodoListSchema[] = data.todos || []
+  // console.log(data);
   return (
-    <div>
-      All Todos
+    <div className="bg-gray-50 min-h-screen">
+      <h1 className="text-center text-2xl font-bold text-blue-800 px-4 pt-6 pb-2">
+        All Todos
+      </h1>
+      <TodoList todos={todos} />
     </div>
   );
 }
