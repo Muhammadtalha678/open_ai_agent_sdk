e53f493af8d65413be5349dc4e4a0a94a0d4de89
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-08-17 19:23:22 +0500

    .

--- nextjs-todo-agentic-ui/src/app/page.tsx
+++ nextjs-todo-agentic-ui/src/app/page.tsx
@@ -5,10 +5,8 @@ import { TodoListSchema } from "@/TypingSchemas/TodoListSchema";
 
 export default async function Home() {
   const data = await apiCallGet(AppRoutes.allTodos)
-
-
   const todos: TodoListSchema[] = data.todos || []
-  // console.log(data);
+  console.log(data);
   return (
     <div className="bg-gray-50 min-h-screen">
       <h1 className="text-center text-2xl font-bold text-blue-800 px-4 pt-6 pb-2">
